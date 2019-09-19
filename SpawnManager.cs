c191097a0c70d4c2accd02f1f06fd969acd518f3
Khathide <47408836+Khathide@users.noreply.github.com>
2019-09-19 16:00:22 +0200

    Upload

---
+++ SpawnManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    [SerializeField]
+    private GameObject _enemyContainer;
+    // Start is called before the first frame update
+    void Start()
+    {
+      StartCoroutine(SpawnRoutine());  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while(true)
+        {
+            float randomXX = Random.Range(-9.16f, 9.16f);
+            Vector3 SpawnPos = new Vector3(randomXX, 7, 0);
+            GameObject newEnemy = Instantiate(_enemyPrefab, SpawnPos, Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
+            yield return new WaitForSeconds(3.0f);
+        }
+    }
+}
