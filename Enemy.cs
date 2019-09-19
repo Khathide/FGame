82672c20722ad1913e57b2710da2dad4b20912a1
Khathide <47408836+Khathide@users.noreply.github.com>
2019-09-19 15:55:39 +0200

    Enemy Object

---
+++ Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] 
+    private float _speed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y < -4f )
+        {
+           float randomX = Random.Range(-9.16f, 9.16f);
+           transform.position = new Vector3(randomX, 7, 0);
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Player player = other.transform.GetComponent<Player>();
+            if (player != null)
+            {
+                player.Damage();
+            }
+            Destroy(this.gameObject);
+        }
+
+        if (other.tag == "Laser")
+        {
+            Destroy(this.gameObject);
+            Destroy(other.gameObject);
+        }
+        //Debug.Log("Hit: " + other.transform.name);
+    }
+}
